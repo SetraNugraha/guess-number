9b897062600431b8ff9b14be01c9cc77d77fca40
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-03-04 16:03:59 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl text-white text-center bg-slate-700 font-bold">Hello React Tailwind !</h1>
+      </div>
+    </>
+  )
+}
